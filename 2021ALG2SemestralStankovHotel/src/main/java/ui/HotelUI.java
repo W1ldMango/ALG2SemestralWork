07cc0bfd832654ee7f01380caeeee09dc6803a89
W1ldMango <79978940+W1ldMango@users.noreply.github.com>
2021-05-21 19:37:50 +0200

    Added a semester job

---
+++ 2021ALG2SemestralStankovHotel/src/main/java/ui/HotelUI.java
@@ -0,0 +1,16 @@
+package ui;
+
+import app.App.App;
+
+import java.io.IOException;
+
+/**
+ * Main UI menu
+ */
+
+public class HotelUI {
+    public static void main(String[] args) throws IOException {
+        App UI = new App();
+        UI.hotelInterface();
+    }
+}
