d00444b55669c2ad8b7cf15dc52e3767ce583cef
W1ldMango <79978940+W1ldMango@users.noreply.github.com>
2021-05-28 15:56:50 +0200

    Added again

---
+++ 2021ALG2SemestralStankovHotel/src/main/java/ui/HotelUI.java
@@ -0,0 +1,18 @@
+package ui;
+
+import app.App.App;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Main UI menu
+ */
+
+public class HotelUI {
+    public static void main(String[] args) throws IOException {
+        App UI = new App();
+        UI.hotelInterface();
+    }
+}
---
+++ 2021ALG2SemestralStankovHotelJava8/src/App/PaymentInfo.java
@@ -0,0 +1,34 @@
+package App;
+
+public class PaymentInfo {
+
+    private String creditCardNum;
+    private String firstname;
+    private String lastname;
+    private String expirationDate;
+    private int cvv;
+
+    public PaymentInfo(String creditCardNum, String firstname, String lastname, String expirationDate,int cvv) {
+        this.creditCardNum = creditCardNum;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.expirationDate = expirationDate;
+        this.cvv = cvv;
+    }
+
+    public PaymentInfo(String firstname,String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "PaymentInfo{" +
+                "creditCardNum='" + creditCardNum + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", expirationDate='" + expirationDate + '\'' +
+                ", cvv='" + cvv + '\'' +
+                '}';
+    }
+}
